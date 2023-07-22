b9f70b644bfe389aa933355415359d3db3c091de
Aubrey Graves <grav0165@gmail.com>
2023-07-22 18:13:01 -0500

    Updated header color, font style.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -29,6 +29,13 @@ function Details() {
     // pulling in useHistory
     const history = useHistory();
 
+    // Creating theme for MUI to use
+    const theme = createTheme({
+        typography: {
+            fontFamily: 'Helvetica'
+        }
+    })
+
     // using function to return to movieList page
     const handleReturn = () => {
         history.push('/')
@@ -38,44 +45,47 @@ function Details() {
     return (
         <div className="details-page">
 
-           
-            <Box sx={{ paddingTop: 10, marginLeft: 3}}>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                    <Card sx={{ width: '90%', minWidth: 240, height: 350, display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: 5, backgroundColor: '#050505' }}>
-                        <CardMedia
-                            title={movieDetails[0]?.title}
-                            sx={{ height: '100%', paddingTop: 3 }}>
-                            <img src={movieDetails[0]?.poster} />
-                        </CardMedia>
-                        <Typography sx={{ paddingBottom: 2, color: '#e8a217'}}>
-                            {movieDetails[0]?.title}
-                        </Typography>
-                    </Card>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: 10 }}>
-                        <Card sx={{ marginBottom: 4, padding: 2, display: 'flex', flexDirection: 'column', backgroundColor: '#050505', color: '#e8a217'}}>
-                            <Typography sx={{ fontSize: 14 }}>
-                                genres
+            <ThemeProvider theme={theme}>
+                <Box sx={{ paddingTop: 10, marginLeft: 3 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                        <Card elevation={6} sx={{ width: '90%', minWidth: 240, height: 350, display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: 5, backgroundColor: '#050505' }}>
+                            {/* Adding ? to all objects inside of details page, as some of the lag from requesting information
+                            from the server was causing severe page breaks */}
+                            <CardMedia
+                                title={movieDetails[0]?.title}
+                                sx={{ height: '100%', paddingTop: 3 }}>
+                                <img src={movieDetails[0]?.poster} />
+                            </CardMedia>
+                            <Typography sx={{ paddingBottom: 2, color: '#e8a217' }}>
+                                {movieDetails[0]?.title}
                             </Typography>
-                            <Box sx={{ display: 'flex', flexDirection: 'row', flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 2}}>
-                            {
-                                movieDetails.map((item) =>
-                                    <Typography sx={{ background: '#561fef', color: 'white', borderRadius: 2, paddingLeft: 1, paddingRight: 1 }}>{item.name}</Typography>
-                                )
-                            }
+                        </Card>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: 10 }}>
+                            <Card elevation={6} sx={{ marginBottom: 4, padding: 2, display: 'flex', flexDirection: 'column', backgroundColor: '#050505', color: '#e8a217' }}>
+                                <Typography sx={{ fontSize: 14 }}>
+                                    genres
+                                </Typography>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 2 }}>
+                                    {
+                                        movieDetails.map((item) =>
+                                            <Typography sx={{ background: '#561fef', color: 'white', borderRadius: 2, paddingLeft: 1, paddingRight: 1 }}>{item.name}</Typography>
+                                        )
+                                    }
+                                </Box>
+                            </Card>
+                            <Card elevation={6} sx={{ padding: 2, backgroundColor: '#050505', color: '#e8a217' }}>
+                                <Typography>
+                                    {movieDetails[0]?.description}
+                                </Typography>
+                            </Card>
                         </Box>
-                        </Card>
-                        <Card sx={{ padding: 2, backgroundColor: '#050505', color: '#e8a217' }}>
-                            <Typography>
-                                {movieDetails[0]?.description}
-                            </Typography>
-                        </Card>
                     </Box>
                 </Box>
-            </Box>
 
-            <Box sx={{ padding: 3 }}>
-                <Button variant="contained" size="large" sx={{ backgroundColor: '#050505', color: '#e8a217'}} onClick={handleReturn}>Return</Button>
-            </Box>
+                <Box sx={{ padding: 3 }}>
+                    <Button elevation={6} variant="contained" size="large" sx={{ backgroundColor: '#050505', color: '#e8a217' }} onClick={handleReturn}>Return</Button>
+                </Box>
+            </ThemeProvider>
         </div>
     )
 }
